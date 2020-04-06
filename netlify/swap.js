1b34904f7593ca5ffae3ee72dc02ba22644b449f
olmedocr <20819857+olmedocr@users.noreply.github.com>
2020-04-06 19:33:39 +0200

    Updated lambda function

--- netlify/swap.js
+++ netlify/swap.js
@@ -1,40 +1,54 @@
-const AWS        = require('aws-sdk');
-const request    = require('request');
-const url        = require('url');
-const encrpytion = require('./encryption');
+const btoa = require('btoa');
+const axios = require('axios');
+const qs = require('qs');
 
-const nop = () => {};
+exports.handler = async (event) => {
+	const config = {
+	    method: "POST",
+	    url: "https://accounts.spotify.com/api/token",
+	    headers: {
+	      "Content-Type": "application/x-www-form-urlencoded",
+	      "Authorization": "Basic " + btoa(process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET)
+	    },
+	    data: {
+	      grant_type: 'authorization_code',
+	      redirect_uri: process.env.SPOTIFY_CLIENT_CALLBACK_URL,
+	      code: event.data.code // authorization code is passed in the body as key "code"
+	    }
+	}
 
-const clientId        = process.CLIENT_ID
-const clientSecret    = process.SPOTIFY_CLIENT_SECRET;
-const clientCallback  = process.SPOTIFY_CALLBACK_URL;
-const encSecret       = process.SPOTIFY_ENCRYPTION_SECRET;
-const authString      = new Buffer(`${clientId}:${clientSecret}`).toString('base64');
-const authorization   = `Basic ${authString}`;
+	try {
+		// url encode data to send to spotify
+		config.data = qs.stringify(config.data)
 
-const spotifyEndpoint = 'https://accounts.spotify.com/api/token';
+		let res = await axios(config);
+		let data = res.data;
 
-exports.swap = (event, context) => {
-  const formData = {
-    grant_type:   'authorization_code',
-    redirect_uri: config.SPOTIFY_CALLBACK_URL,
-    code:         event.code,
-  };
-  const options = {
-    uri:     url.parse(spotifyEndpoint),
-    headers: { 'Authorization' : authorization },
-    form:   formData,
-    method:  'POST',
-    json:   true
-  };
-
-  return new Promise((resolve, reject) => {
-    request(options, (error, response, body) => {
-      if (response.statusCode === 200) {
-        body.refresh_token = encrpytion.encrypt(body.refresh_token, encSecret);
-      }
-      resolve(body);
-    });
-  }).then(value => context.succeed(value))
-    .catch(e => context.fail(JSON.stringify(e)));
+		return data;
+	} catch(err) {
+		console.log(err);
+		return err;
+	}
 };
+
+/*
+API Gateway Mapper for application/x-www-form-urlencoded
+
+{
+    "data": {
+        #foreach( $token in $input.path('$').split('&') )
+            #set( $keyVal = $token.split('=') )
+            #set( $keyValSize = $keyVal.size() )
+            #if( $keyValSize >= 1 )
+                #set( $key = $util.urlDecode($keyVal[0]) )
+                #if( $keyValSize >= 2 )
+                    #set( $val = $util.urlDecode($keyVal[1]) )
+                #else
+                    #set( $val = '' )
+                #end
+                "$key": "$val"#if($foreach.hasNext),#end
+            #end
+        #end
+    }
+}
+*/
